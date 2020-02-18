e4ae028614dd7e570c014c48b9445d200c98d849
NickChiapputo <chiapputo.nick@gmail.com>
2020-02-18 16:46:05 -0600

    Fixed canny not getting gaussian blurring mat.

--- ComputerVisionTest/GrooveDetection/grooveDetection.cpp
+++ ComputerVisionTest/GrooveDetection/grooveDetection.cpp
@@ -16,7 +16,7 @@ int main( int argc, char ** argv )
 
 
 	// Create matrix to hold image for each step
-	Mat image, gray, gaussian, canny, cannyOverlay, hough, linesHP, houghP;
+	Mat image, gray, gaussian, canny, cannyOverlay, hough, linesHP, houghP, grouped;
 
 
 	// Create an image and read from the given source
@@ -35,15 +35,15 @@ int main( int argc, char ** argv )
 
 
 	// Gaussian Blurring with 5x5 Gaussian kernel
-	int kernel = 13;
+	int kernel = 3;
 	GaussianBlur( gray, gaussian, Size( kernel, kernel ), 0 );
 
 
 	// Canny image detection
-	int low_threshold = 100;
+	int low_threshold = 10;
 	int high_threshold = 150;
 	int apertureSize = 3;
-	Canny( image, canny, low_threshold, high_threshold, apertureSize );
+	Canny( gaussian, canny, low_threshold, high_threshold, apertureSize );
 
 
 	// Overlay canny on source image
@@ -74,6 +74,8 @@ int main( int argc, char ** argv )
 	HoughLines( canny, lines, 1, theta, threshold, srn, stn );
 
 	// Draw Hough lines
+	Scalar color = Scalar( 0, 0, 255 );
+	int thickness = 2;
 	for( size_t i = 0; i < lines.size(); i++ )
 	{
 		float rho = lines[ i ][ 0 ];
@@ -93,7 +95,7 @@ int main( int argc, char ** argv )
 		pt2.x = cvRound( x0 - 1000 * ( -b ) );
 		pt2.y = cvRound( y0 - 1000 * (  a ) );
 
-		line( hough, pt1, pt2, Scalar( 0, 0, 255 ), 3, LINE_AA );
+		line( hough, pt1, pt2, color, thickness, LINE_AA );
 	} 
 
 
@@ -103,22 +105,87 @@ int main( int argc, char ** argv )
 	double rhoP = 1;					// Check in small rho increments
 	double thetaP = 1 * CV_PI / 180;	// Check in small theta increments
 	int thresholdP = 100;				// Number of points needed to detect lines. Gets rid of sipes and block borders
-	double minLineLength = 400;			// Only check for long lines
-	double maxLineGap = 500;			// Allow large gaps between lines. This overcomes the issue of blocks disrupting the grooves
+	double minLineLength = 0.5 * cannyOverlay.rows;			// Only check for long lines (>= 90% of height)
+	double maxLineGap = 0.15 * cannyOverlay.cols;			// Allow large gaps between lines. This overcomes the issue of blocks disrupting the grooves
 	HoughLinesP( canny, linesP, rhoP, thetaP, thresholdP, minLineLength, maxLineGap );
 
+	double averageTheta = 0;
+
+	vector<Vec4i> selectedPLines;
+
 	// Draw Hough Probabilistic Lines
-	Scalar color = Scalar( 0, 0, 255 );
 	for( size_t i = 0; i < linesP.size(); i++ )
 	{
 		Vec4i l = linesP[ i ];
 
+		// Calculate angle compared to bottom of window
+		double hypotenuseLength = sqrt( ( abs( l[ 0 ] - l[ 2 ] ) ) * ( abs( l[ 0 ] - l[ 2 ] ) ) + ( abs( l[ 1 ] - l[ 3 ] ) ) * ( abs( l[ 1 ] - l[ 3 ] ) ) );
+		averageTheta += asin( abs( l[ 3 ] - l[ 1 ] ) / hypotenuseLength ) * 180 / ( CV_PI * linesP.size() );
+
+		// cout 	<< "( " << l[ 0 ] << ", " << l[ 1 ] << " ) -> ( " << l[ 2 ] << ", " << l[ 3 ] << " )\nTheta = sin^(-1)( " 
+		// 		<< abs( l[ 3 ] - l[ 1 ] ) << " / " << hypotenuseLength << " ) = " << asin( abs( l[ 3 ] - l[ 1 ] ) / hypotenuseLength ) * 180 / CV_PI << "\n\n";
+
 		// Only check for vertical lines whose bottom and top X values are within 20 of each other
 		if( abs( l[ 0 ] - l[ 2 ] ) < 20 )
 		{
-			line( houghP, Point( l[ 0 ], l[ 1 ] ), Point( l[ 2 ], l[ 3 ] ), color, 3, LINE_AA );
+			selectedPLines.push_back( Vec4i( l[ 0 ], l[ 1 ], l[ 2 ], l[ 3 ] ) );
+
+			cout << "Push Back: ( " << l[ 0 ] << ", " << l[ 1 ] << " ) -> ( " << l[ 2 ] << ", " << l[ 3 ] << "\n";
+
+			line( houghP, Point( l[ 0 ], l[ 1 ] ), Point( l[ 2 ], l[ 3 ] ), color, thickness, LINE_AA );
 		}
 	}
+	// cout << "Average Theta: " << averageTheta << "\n";
+
+
+	// Find groups of houghP lines and average them together
+	// grouped = cannyOverlay.clone();
+	// for( size_t i = 0; i < selectedPLines.size(); i++ )
+	// {
+	// 	Vec4i currentLine = selectedPLines[ i ];
+
+	// 	double currMedian = ( currentLine[ 0 ] + currentLine[ 2 ] ) / 2;
+
+	// 	Vec4i avgLine = Vec4i( 0, 0, 0, 0 );
+	// 	int numLines = 0;
+	// 	for( size_t j = i + 1; j < selectedPLines.size(); j++ )
+	// 	{
+	// 		Vec4i compareLine = selectedPLines[ j ];
+	// 		double compareMedian = ( compareLine[ 0 ] + compareLine[ 2 ] ) / 2;
+
+	// 		// If center of lines is within 20 pixels
+	// 		if( abs( compareMedian - currMedian ) > 20 )
+	// 		{
+	// 			cout << "( " << compareLine[ 0 ] << ", " << compareLine[ 1 ] << " ) -> ( " << compareLine[ 2 ] << ", " << compareLine[ 3 ] << " )\n";
+
+	// 			numLines++;
+	// 			avgLine[ 0 ] += compareLine[ 0 ];
+	// 			avgLine[ 1 ] += compareLine[ 1 ];
+	// 			avgLine[ 2 ] += compareLine[ 2 ];
+	// 			avgLine[ 3 ] += compareLine[ 3 ];
+
+	// 			// selectedPLines.erase( selectedPLines.begin() + j );
+	// 		}
+	// 	}
+
+	// 	if( numLines != 0 )
+	// 	{
+	// 		cout << avgLine[ 0 ] << " / " << numLines << " = ";
+	// 		avgLine[ 0 ] = avgLine[ 0 ] / numLines;
+	// 		cout << avgLine[ 0 ] << "\n" << avgLine[ 1 ] << " / " << numLines << " = ";
+	// 		avgLine[ 1 ] = avgLine[ 1 ] / numLines;
+	// 		cout << avgLine[ 1 ] << "\n" << avgLine[ 2 ] << " / " << numLines << " = ";
+	// 		avgLine[ 2 ] = avgLine[ 2 ] / numLines;
+	// 		cout << avgLine[ 2 ] << "\n" << avgLine[ 3 ] << " / " << numLines << " = ";
+	// 		avgLine[ 3 ] = avgLine[ 3 ] / numLines;
+	// 		cout << avgLine[ 3 ] << "\n";
+
+	// 		cout << "( " << avgLine[ 0 ] << ", " << avgLine[ 1 ] << " ) -> ( " << avgLine[ 2 ] << ", " << avgLine[ 3 ] << " )\n\n";
+
+	// 		line( grouped, Point( avgLine[ 0 ], avgLine[ 1 ] ), Point( avgLine[ 2 ], avgLine[ 3 ] ), color, thickness, LINE_AA );
+	// 	}
+	// }
+
 
 	// Create window to display image in
 	namedWindow( "Display", WINDOW_AUTOSIZE );
@@ -159,6 +226,11 @@ int main( int argc, char ** argv )
 			cout << "Change Display: Probabilistic Hough Lines\n";
 			imshow( "Display", houghP );
 		}
+		// else if( key == 55 )	// Key press = '7'
+		// {
+		// 	cout << "Change Display: Probabilistic Hough Lines with grouped lines\n";
+		// 	imshow( "Display", grouped );
+		// }
 	}
 
 	// Save image file
